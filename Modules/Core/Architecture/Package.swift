f4166214d5c4a329c06754adcec6242885d3ac69
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2024-12-14 15:06:42 +0900

    Style: Auto Lint

--- Modules/Core/Architecture/Package.swift
+++ Modules/Core/Architecture/Package.swift
@@ -18,5 +18,4 @@ let package = Package(
       name: "Architecture",
       dependencies: [
       ]),
-  ]
-)
+  ])
--- Modules/Core/Platform/Package.swift
+++ Modules/Core/Platform/Package.swift
@@ -18,5 +18,4 @@ let package = Package(
       name: "Platform",
       dependencies: [
       ]),
-  ]
-)
+  ])
